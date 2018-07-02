90906b7564cf7961ac548b3ab8abd5229906a388
Yunjun Wang <yunjun.wangirl@gmail.com>
2018-07-02 11:00:13 +0100

    FindMaxBinaryGap.java
    finding the max binary gap in a given number
    e.g. 100101 the max binary gap is 2

---
+++ FindMaxBinaryGap.java
@@ -0,0 +1,25 @@
+/**
+*finding max binary gap in a given number
+*
+*/
+public class FindMaxBinaryGap {
+
+	public int findMaxBinaryGap(int N){
+		
+		String bString = Integer.toBinaryString(N);
+		char[] c = bString.toCharArray();
+		int gap = 0;
+		int maxGap = 0;
+		
+		for(int i = 0; i < c.length; i++){
+			if(c[i] == '0'){
+				gap++;
+			}else{
+				maxGap = (maxGap >= gap) ? maxGap : gap;
+				gap = 0;
+			}
+		}
+		
+		return maxGap;
+	}
+}
